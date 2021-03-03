e28a5f385ce5c161f5e9938fa1427dd68543ca69
r.karmaev <karmaev05@gmail.com>
2021-03-03 11:30:50 +0700

    Создание директорий для реализации патерна MVVM и изменение началной позиции окна

--- TestWorkASU/View/MainWindow.xaml.cs
+++ TestWorkASU/View/MainWindow.xaml.cs
@@ -14,10 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TestWorkASU
-{
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+{ 
     public partial class MainWindow : Window
     {
         public MainWindow()
