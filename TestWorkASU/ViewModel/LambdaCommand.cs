ba8b5142e3150fd4a3c75379a522fad2a325ea08
r.karmaev <karmaev05@gmail.com>
2021-03-03 12:10:53 +0700

    Добавление классов команд и ViewModel

---
+++ TestWorkASU/ViewModel/LambdaCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestWorkASU.ViewModel.Base;
+
+namespace TestWorkASU.ViewModel
+{
+    class LambdaCommand : ButtonCommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+        public LambdaCommand(Action<object> Execute, Predicate<object> CanExecute = null)
+        {
+            _execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _canExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _execute(parameter);
+    }
+}
