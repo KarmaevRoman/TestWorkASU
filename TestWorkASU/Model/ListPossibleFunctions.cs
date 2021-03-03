1fed4b180a41f271e2a901a384f5995b1c7ae724
r.karmaev <karmaev05@gmail.com>
2021-03-03 12:57:37 +0700

    Добавление класса и привязка коллекции возможных решений

---
+++ TestWorkASU/Model/ListPossibleFunctions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWorkASU.Model
+{
+    class ListPossibleFunctions
+    {
+        private ObservableCollection<string> listFunctions;
+        public ObservableCollection<string> ListFunctions
+        {
+            get => listFunctions;
+
+            set => listFunctions = value;
+        }
+
+        public ListPossibleFunctions()
+        {
+            listFunctions = new ObservableCollection<string> { "линейная", "квадратичная", "кубическая", "4-ой степени", "5-ой степени" };
+        }
+    }
+}
--- TestWorkASU/ViewModel/MainViewModel.cs
+++ TestWorkASU/ViewModel/MainViewModel.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestWorkASU.ViewModel.Base;
+using TestWorkASU.Model;
+using System.Collections.ObjectModel;
 
 namespace TestWorkASU.ViewModel
 {
-    class MainViewModel
+    class MainViewModel : BaisViewModel
     {
+        private ObservableCollection<string> listFunctions;
+        public ObservableCollection<string> ListFunctions
+        {
+            get => listFunctions;
+
+            set => Set(ref listFunctions, value);
+        }
+
+        public MainViewModel()
+        {
+            ListFunctions = new ListPossibleFunctions().ListFunctions;
+        }
     }
 }
